527d382aa5461491c8fba9aaccbfbbe955b926b1
Devesh Kumar <devesh2027@gmail.com>
2025-03-25 18:52:36 +0530

    sdk: Add type definitions for events we want to track

--- sdk/src/types/index.ts
+++ sdk/src/types/index.ts
@@ -1,5 +1,41 @@
-type UserInteractionEvent = {};
+export type ScaffoldingEvent = {
+	type: "scaffolding";
+	html: string;
+};
 
-type DOMMutationEvent = {};
+export type MouseMoveInteractionEvent = {
+	type: "mousemove";
+	x: number;
+	y: number;
+};
 
-export type UserEvent = DOMMutationEvent | UserInteractionEvent;
+export type MouseClickEvent = {
+	type: "mouseclick";
+	x: number;
+	y: number;
+};
+
+export type InputEvent = {
+	type: "input";
+	target: string;
+	newValue: string;
+};
+
+export type DOMMutationEvent = {
+	type: "mutation";
+	subType: "attributes" | "characterData" | "childList";
+	target: string;
+	attributeName: string | null;
+	oldValue: string | null;
+	newValue: string | null;
+	addedNodes: (string | null)[];
+	removedNodes: (string | null)[];
+};
+
+export type UserEvent = { time: number } & (
+	| ScaffoldingEvent
+	| MouseMoveInteractionEvent
+	| MouseClickEvent
+	| InputEvent
+	| DOMMutationEvent
+);
